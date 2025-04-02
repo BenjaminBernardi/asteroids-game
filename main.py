da248c4ea6dfe2928a697fffedb49e38b01b4a07
benjaminbernardi <benjamin.bernardi@outlook.com>
2025-04-02 15:54:02 +0200

    save progress 4

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -10,12 +12,25 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         screen.fill(color="black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         for event in pygame.event.get():
